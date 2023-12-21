df8e06005883b13b24310d6a3108a26875685448
glassmiller <glassmmilani@gmail.com>
2023-12-21 18:12:44 -0300

    Exercicio: Solução 1

---
+++ Exercicio/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿// Index.cshtml.cs
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class IndexModel : PageModel
+{
+    [BindProperty]
+    public decimal? CustoFinal { get; set; }
+
+    public IActionResult OnPost(decimal custoFabrica)
+    {
+        // Constantes para porcentagens
+        const decimal percentualDistribuidor = 0.28m;
+        const decimal percentualImpostos = 0.45m;
+
+        // Calcular o custo final
+        decimal custoDistribuidor = custoFabrica * percentualDistribuidor;
+        decimal custoImpostos = custoFabrica * percentualImpostos;
+        CustoFinal = custoFabrica + custoDistribuidor + custoImpostos;
+
+        return Page();
+    }
+}
